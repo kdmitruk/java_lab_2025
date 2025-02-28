d9eddeb62365f5370b0455219f3b8c798c554035
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-02-28 09:22:12 +0100

    zadanie 1

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Point p = new Point();
+        p.x = 5.0;
+        p.y = 7.5;
+
+//        System.out.println("X = " + p.x + " Y = " + p.y);
+        System.out.println(p);
     }
 }
---
+++ svg/src/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+    public double x;
+    public double y;
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+//    public String toString() {
+//        return "X = " + x + "\nY = " + y;
+//    }
+}
