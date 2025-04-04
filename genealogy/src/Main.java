2217c2e1ea652835ab03ac08aad55bd823f24f34
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-04-04 08:21:24 +0200

    Lab 5 zad 2

--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+//        Person person = new Person("Adam","Nowak",LocalDate.of(2000,5,21));
+//        List<Person>people = new ArrayList<>();
+//        people.add(person);
+//        people.add(new Person("Pawel","Kowalski",LocalDate.of(2003,2,10)));
+//        for (Person value : people) {
+//            System.out.println(value);
+//        }
         Person person = new Person("Adam","Nowak",LocalDate.of(2000,5,21));
-        List<Person>people = new ArrayList<>();
-        people.add(person);
-        people.add(new Person("Pawel","Kowalski",LocalDate.of(2003,2,10)));
-        for (Person value : people) {
-            System.out.println(value);
-        }
-
+        Person child = new Person("Pawel","Kowalski",LocalDate.of(2003,2,10));
+        System.out.println(person.adopt(child));
+        System.out.println(person.adopt(child));
+        System.out.println(person);
     }
 }
--- genealogy/src/Person.java
+++ genealogy/src/Person.java
@@ -1,12 +1,30 @@
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Person {
     private String firstName,lastName;
     private LocalDate birthDay;
+    private Set<Person> children;
 
     public Person(String firstName, String lastName, LocalDate birthDay) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDay = birthDay;
+        this.children = new HashSet<>();
+    }
+    public boolean adopt(Person child)
+    {
+        return this.children.add(child);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDay=" + birthDay +
+                ", children=" + children +
+                '}';
     }
 }
